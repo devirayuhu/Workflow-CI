399c854ac637ecd4e1a6a5ee9780b77bf0e0081c
devirayuhu <deviranugraha4@gmail.com>
2025-12-20 22:54:38 +0700

    Initial CI workflow with MLflow

---
+++ MLProject/modelling.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+print("Load dataset")
+df = pd.read_csv("churn_preprocessing.csv")
+
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+print("Training model")
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", acc)
+print("Training selesai")
